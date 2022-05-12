709f70667d95f65276e4c4c9fd6f02b65a70214c
Julian Janik <janikj@bzz.ch>
2022-05-12 15:00:44 +0200

    initial commit

---
+++ src/main/java/ch/bzz/bookshelf/model/Book.java
@@ -0,0 +1,63 @@
+package ch.bzz.bookshelf.model;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private String bookUUID;
+    private String title;
+
+    public String getBookUUID() {
+        return bookUUID;
+    }
+
+    public void setBookUUID(String bookUUID) {
+        this.bookUUID = bookUUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    private String author;
+    private Publisher publisher;
+    private BigDecimal price;
+    private String ISBN;
+
+
+}
